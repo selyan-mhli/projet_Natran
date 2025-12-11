0f00706981eefd38bc14a45021e03f9a56b0b296
Selyan Mouhali <selyan.mouhali@gmail.com>
2025-12-11 19:29:21 +0100

    Configure base path for GitHub Pages
    Set base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/projet_Natran/',   // IMPORTANT pour GitHub Pages
   plugins: [react()],
   server: {
     port: 3000,

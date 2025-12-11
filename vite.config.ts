220f551fc29a34bac368fc8624d4b24fa2c00090
Moss'Ab Mirande-Ney <mossabmirande-ney@wifi-etudiants1139.utt.fr>
2025-12-11 19:51:28 +0100

    fix: corriger chemins pour GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/projet_Natran/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   server: {
     port: 3000,
     open: true

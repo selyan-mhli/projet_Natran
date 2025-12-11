6d44004e20d5e21796409564210834b89e248df9
Moss'Ab Mirande-Ney <mossabmirande-ney@wifi-etudiants1139.utt.fr>
2025-12-11 19:46:09 +0100

    feat: ajouter GitHub Actions pour déploiement automatique

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/projet_Natran/',   // IMPORTANT pour GitHub Pages
   plugins: [react()],
+  base: '/projet_Natran/',
   server: {
     port: 3000,
     open: true
